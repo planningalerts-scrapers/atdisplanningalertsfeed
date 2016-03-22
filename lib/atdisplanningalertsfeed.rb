72b60bf7f609a86f8e1defd4ca8a71f7a7a6f73d
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-03-22 14:44:29 +1030

    Refactor to use a logger for the moment instead of straight puts

--- lib/atdisplanningalertsfeed.rb
+++ lib/atdisplanningalertsfeed.rb
@@ -6,28 +6,33 @@ require 'cgi'
 module ATDISPlanningAlertsFeed
   def self.save(url, options = {})
     feed = ATDIS::Feed.new(url)
+    logger = options[:logger]
+    logger ||= Logger.new(STDOUT)
+
     options[:lodgement_date_start] = (options[:lodgement_date_start] || Date.today - 30)
     options[:lodgement_date_end] = (options[:lodgement_date_end] || Date.today)
     page = feed.applications(lodgement_date_start: options[:lodgement_date_start], lodgement_date_end: options[:lodgement_date_end])
 
     # Save the first page
     pages_processed = []
-    pages_processed << page.pagination.current if save_page(page)
+    pages_processed << page.pagination.current if save_page(page, logger)
+
+    logger.debug("Fetching #{page.next_url}")
 
     while page = page.next_page
       # Some ATDIS feeds incorrectly provide pagination
       # and permit looping; so halt processing if we've already processed this page
       unless pages_processed.index(page.pagination.current).nil?
-        puts "Page #{page.pagination.current} already processed; halting"
+        logger.info("Page #{page.pagination.current} already processed; halting")
         break
       end
 
-      pages_processed << page.pagination.current if save_page(page)
+      pages_processed << page.pagination.current if save_page(page, logger)
     end
   end
 
-  def self.save_page(page)
-    puts "Saving page #{page.pagination.current} of #{page.pagination.pages}"
+  def self.save_page(page, logger)
+    logger.info("Saving page #{page.pagination.current} of #{page.pagination.pages}")
 
     page.response.each do |item|
       application = item.application
