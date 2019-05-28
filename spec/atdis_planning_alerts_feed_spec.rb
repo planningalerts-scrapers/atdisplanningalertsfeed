196a08f8b8951f9170e3c41af84139188cb43afd
Matthew Landauer <mlandauer@gmail.com>
2019-05-28 11:56:04 +1000

    Modernise and refactor tests

--- spec/atdis_planning_alerts_feed_spec.rb
+++ spec/atdis_planning_alerts_feed_spec.rb
@@ -18,78 +18,91 @@ VCR.configure do |c|
 end
 
 describe ATDISPlanningAlertsFeed, :vcr do
-  before :each do
-    @options = {
+  let(:options) do
+    {
       lodgement_date_start: Date.parse("2016-02-21"),
       lodgement_date_end: Date.parse("2016-03-22"),
       # Make the tests run quietly
       logger: Logger.new("/dev/null")
     }
   end
-  context "valid feed" do
-    it "should not error on empty feed" do
-      records = ATDISPlanningAlertsFeed.save(
-        "http://mycouncil2.solorient.com.au/Horizon/@@horizondap_ashfield@@/atdis/1.0/",
-        @options
-      )
 
+  context "valid feed" do
+    let(:records) do
+      ATDISPlanningAlertsFeed.save(
+        "http://mycouncil2.solorient.com.au/Horizon/@@horizondap_ashfield@@/atdis/1.0/",
+        options
+      )
+    end
+
+    it "should not error on empty feed" do
       expect(records.length).to eq 0
     end
   end
-  context "dodgy pagination" do
-    it "should not error" do
-      records = ATDISPlanningAlertsFeed.save(
-        "https://myhorizon.maitland.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
-        @options
-      )
 
+  context "dodgy pagination" do
+    let(:records) do
+      ATDISPlanningAlertsFeed.save(
+        "https://myhorizon.maitland.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
+        options
+      )
+    end
+
+    it "should not error" do
       expect(records.length).to eq 120
     end
   end
 
   context "really dodgy pagination" do
-    it "should not error" do
-      records = ATDISPlanningAlertsFeed.save(
+    let(:records) do
+      ATDISPlanningAlertsFeed.save(
         "https://da.kiama.nsw.gov.au/atdis/1.0/",
-        @options
+        options
       )
+    end
 
+    it "should not error" do
       expect(records.length).to eq 43
     end
   end
 
-  context "with a flakey service" do
+  context "with a flakey service cootamundra" do
+    let(:records) do
+      ATDISPlanningAlertsFeed.save(
+        "http://myhorizon.cootamundra.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
+        options.merge(flakey: true)
+      )
+    end
+
     # TODO: This spec should always force a
     # RestClient::InternalServerError: 500 Internal Server Error
     it "should not error" do
-      @options.merge!(flakey: true)
       # TODO: This doesn't work as expected (stackleveltoodeep), but the VCR cassette should work
       # allow_any_instance_of(ATDIS::Feed).to
       # receive(:applications).and_raise(
       #   RestClient::InternalServerError.new("500 Internal Server Error"))
-
-      url = "http://myhorizon.cootamundra.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/"
-      records = ATDISPlanningAlertsFeed.save(url, @options)
-
       # TODO: Expectation that a HTTP 500 on the first page recovers gracefully
       expect(records.length).to eq 0
     end
+  end
+
+  context "with a flakey service yass" do
+    let(:records) do
+      # Yass isn't actually flakey, but Cootamundra is *too* flakey
+      # This scenario replicates one page of many having an unhandled exception
+      # (seen in Horizon DAP feeds)
+      ATDISPlanningAlertsFeed.save(
+        "http://mycouncil.yass.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
+        options.merge(flakey: true)
+      )
+    end
 
     it "should not error half way through processing" do
-      @options.merge!(flakey: true)
-
       # TODO: This doesn't work as expected
       # But I have faked the response in the cassette
       # allow_any_instance_of(ATDIS::Models::Page).to
       # receive(:next_page).and_raise(
       #   RestClient::InternalServerError.new("500 Internal Server Error"))
-
-      # Yass isn't actually flakey, but Cootamundra is *too* flakey
-      # This scenario replicates one page of many having an unhandled exception
-      # (seen in Horizon DAP feeds)
-      url = "http://mycouncil.yass.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/"
-      records = ATDISPlanningAlertsFeed.save(url, @options)
-
       # TODO: Expectation that a HTTP 500 on the second page still allows several errors to process
       expect(records.length).to eq 20
     end
